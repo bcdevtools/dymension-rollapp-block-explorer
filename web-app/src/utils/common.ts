5403ecd93b4c066ce14ee091b18e9c75f1d909ed
Teddy Nguyen <ducnh251092@gmail.com>
2024-04-09 00:37:43 +0700

    Update common.ts

--- web-app/src/utils/common.ts
+++ web-app/src/utils/common.ts
@@ -1,4 +1,11 @@
+import {
+  DEFAULT_DATE_TIME_FORMAT,
+  DEFAULT_PAGINATION_SIZE,
+  MAX_PAGINATION_SIZE,
+} from '@/consts/setting';
 import dayjs from 'dayjs';
+
+export type SearchParam = string | string[] | undefined;
 
 export function getRollappPathFromPathname(pathname: string) {
   return pathname.match(/^\/[^\/]*/)![0];
@@ -10,5 +17,27 @@ export function getNewPathByRollapp(pathname: string, newPath: string) {
 }
 
 export function formatUnixTime(unixTime: number) {
-  return dayjs.unix(unixTime).format('YYYY-MM-DD HH:mm:ss');
+  return dayjs.unix(unixTime).format(DEFAULT_DATE_TIME_FORMAT);
+}
+
+export function getStringParamAsArrayNumber(param: SearchParam): number[] {
+  if (!param) return [];
+  const numbers = Array.isArray(param)
+    ? param.map(parseInt)
+    : [parseInt(param)];
+  return numbers.filter(v => !isNaN(v));
+}
+
+export function getStringParamAsNumber(param: SearchParam): number {
+  return param ? parseInt(param.toString()) : NaN;
+}
+
+export function getValidPageSize(pageSize: number) {
+  if (isNaN(pageSize) || pageSize <= 0) return DEFAULT_PAGINATION_SIZE;
+  return Math.min(pageSize, MAX_PAGINATION_SIZE);
+}
+
+export function getOffsetFromPageAndPageSize(page: number, pageSize: number) {
+  if (isNaN(page) || page < 0) return 0;
+  return page * pageSize;
 }
