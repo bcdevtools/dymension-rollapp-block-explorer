4c73d163847dc942893c180836b7eca8655b15a3
Teddy Nguyen <ducnh251092@gmail.com>
2024-04-09 14:44:24 +0700

    Update index.tsx

--- web-app/src/components/client/block/BlockListPage/index.tsx
+++ web-app/src/components/client/block/BlockListPage/index.tsx
@@ -3,7 +3,7 @@
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import { Skeleton } from '@mui/material';
+import Skeleton from '@mui/material/Skeleton';
 import { useLatestBlock } from '@/hooks/useLatestBlock';
 import BlockListTable from './_BlockListTable';
 
