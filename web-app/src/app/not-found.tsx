68ecd64c07a284b1db88c54ba093d0b67e3c4969
Teddy Nguyen <ducnh251092@gmail.com>
2024-04-12 14:13:09 +0700

    Update not-found.tsx

--- web-app/src/app/not-found.tsx
+++ web-app/src/app/not-found.tsx
@@ -1,5 +1,19 @@
 import Error from '@/components/client/commons/Error';
+import Link from '@mui/material/Link';
 
 export default function NotFound() {
-  return <Error statusCode={404} />;
+  return (
+    <Error
+      statusCode={404}
+      //@ts-ignore
+      title={
+        <>
+          Could not find requested resource.{' '}
+          <Link href="/" underline="hover">
+            Return Home
+          </Link>
+        </>
+      }
+    />
+  );
 }
