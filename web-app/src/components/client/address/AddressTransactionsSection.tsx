5b4571971323ba21ecfba3c6d08649a73c55e25c
Teddy Nguyen <ducnh251092@gmail.com>
2024-04-14 23:05:44 +0700

    Update AddressTransactionsSection.tsx

--- web-app/src/components/client/address/AddressTransactionsSection.tsx
+++ web-app/src/components/client/address/AddressTransactionsSection.tsx
@@ -7,7 +7,6 @@ import {
   AddressPageSearchParams,
   AddressTransactionType,
 } from '@/consts/addressPage';
-import Box from '@mui/material/Box';
 import { usePathname, useRouter } from 'next/navigation';
 
 const transactionTabs = [
@@ -34,24 +33,23 @@ export default function AddressTransactionsSection({
 
   return (
     <Card>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs
-          value={txType}
-          onChange={(e, newValue) => {
-            const newSearchParams = new URLSearchParams(
-              newValue && {
-                [AddressPageSearchParams.TX_TYPE]: newValue,
-              }
-            );
-            router.push(`${pathname}?${newSearchParams.toString()}`, {
-              scroll: false,
-            });
-          }}>
-          {transactionTabs.map(({ label, value }) => (
-            <Tab key={value} label={label} value={value} />
-          ))}
-        </Tabs>
-      </Box>
+      <Tabs
+        value={txType}
+        sx={{ mb: 2 }}
+        onChange={(e, newValue) => {
+          const newSearchParams = new URLSearchParams(
+            newValue && {
+              [AddressPageSearchParams.TX_TYPE]: newValue,
+            }
+          );
+          router.push(`${pathname}?${newSearchParams.toString()}`, {
+            scroll: false,
+          });
+        }}>
+        {transactionTabs.map(({ label, value }) => (
+          <Tab key={value} label={label} value={value} />
+        ))}
+      </Tabs>
       {children}
     </Card>
   );
