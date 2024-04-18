2c15b9d20cfd7a1b769cc9603c81062c044a06c7
0xbcdev <0xbcdev@bcdev.tools>
2024-04-18 21:47:27 +0700

    refactor extract body

--- web-app/src/components/client/transaction/TransactionListTable.tsx
+++ web-app/src/components/client/transaction/TransactionListTable.tsx
@@ -15,6 +15,7 @@ import Chip from '@mui/material/Chip';
 import { Path } from '@/consts/path';
 import { formatUnixTime } from '@/utils/datetime';
 import { getMessageName } from '@/utils/transaction';
+import { func } from 'prop-types';
 
 type TransactionListTableProps = Readonly<{
   transactions: Required<{
@@ -54,9 +55,30 @@ export default function TransactionListTable({
     setLoading(false);
   }, [transactions]);
 
+  const getMessageLabel = function(hash:string, message_types:string[], action:string|null, tx_type:string) {
+    if (action) {
+      const splitted = action.split(':');
+      const label = splitted[1] || splitted[0];
+      if (label) {
+        if (tx_type === 'evm') {
+          return <Chip key={hash} label={label} color="info" variant="outlined" />;
+        } else if (tx_type === 'wasm') {
+          return <Chip key={hash} label={label} color="secondary" variant="outlined" />;
+        }
+      }
+    }
+
+    return message_types.map((i, idx) => (
+      <React.Fragment key={idx}>
+        <Chip label={getMessageName(i)} color="default" variant="outlined" />
+        {idx + 1 !== message_types.length && <br />}
+      </React.Fragment>
+    ));
+  }
+
   const body = transactions.map(
     ({ hash, epoch, message_types, action, tx_type, height }) => {
-      let cells = [];
+      const cells = [];
 
       // Transaction Hash
 
@@ -69,29 +91,7 @@ export default function TransactionListTable({
 
       // Messages
 
-      let messages;
-      if (action) {
-        const splitted = action.split(':');
-        const label = splitted[1] || splitted[0];
-        if (label) {
-          if (tx_type === 'evm') {
-            messages = <Chip key={hash} label={label} color="info" variant="outlined" />;
-          } else if (tx_type === 'wasm') {
-            messages = <Chip key={hash} label={label} color="secondary" variant="outlined" />;
-          }
-        }
-      }
-
-      if (!messages) {
-        messages = message_types.map((i, idx) => (
-          <React.Fragment key={idx}>
-            <Chip label={getMessageName(i)} color="default" variant="outlined" />
-            {idx + 1 !== message_types.length && <br />}
-          </React.Fragment>
-        ));
-      }
-
-      cells.push(messages);
+      cells.push(getMessageLabel(hash, message_types, action, tx_type));
 
       // Block height
 
