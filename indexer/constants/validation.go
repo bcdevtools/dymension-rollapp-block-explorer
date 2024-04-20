5e4f7733e011c7bac3d72c28338ccc7f57093714
0xbcdev <0xbcdev@bcdev.tools>
2024-04-20 23:33:03 +0700

    add more basic validation for `be_getTransactionsInBlockRange`

---
+++ indexer/constants/validation.go
@@ -0,0 +1,12 @@
+package constants
+
+const (
+	RpcTxAheadBlockTimeAllowed                     = 86400 - 1 // now + X
+	RpcTxMaximumTxsPerBlockAllowed                 = 20        // maximum X txs per block
+	RpcTxMaximumMessagesPerTxAllowed               = 30        // maximum X msgs per tx
+	RpcTxMaximumStringSizeAllowed                  = 1000      // characters
+	RpcTxMaximumSignersPerTx                       = 10        // maximum X signers per tx
+	RpcTxMaximumLengthAddressAllowed               = 100       // address characters can not access X characters
+	RpcTxMaximumInvolversPerTxAllowed              = 200       // no more than X involvers can be involved in one tx
+	RpcTxMaximumTokenContractsInvolvesPerTxAllowed = 20        // no more than X token contracts can be involved in one tx
+)
--- indexer/services/query/types/errors.go
+++ indexer/services/query/types/errors.go
@@ -8,6 +8,7 @@ import (
 // ErrBlackList is the error when the response contains critical issue
 var ErrBlackList = errors.New("black list")
 var ErrBlackListDueToMisMatchChainId = errors.Wrap(ErrBlackList, "mis-match chain-id")
+var ErrBlackListDueToMalformedResponse = errors.Wrapf(ErrBlackList, "malformed response")
 
 // IsErrBlackList returns true if the error indicate the response contains critical issue, the URL should be ignored
 func IsErrBlackList(err error) bool {
