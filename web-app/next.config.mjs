f23695a30d3f4b2e11e3ed7e426e1fc6e1262a14
0xbcdev <0xbcdev@bcdev.tools>
2024-04-28 14:31:13 +0700

    Revert "add header Content-Security-Policy: upgrade-insecure-requests"
    This reverts commit 89a562fef895e49c09ff213ae5234e43eb0220f4.

--- web-app/next.config.mjs
+++ web-app/next.config.mjs
@@ -6,23 +6,8 @@ const withBundleAnalyzer = NextBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const cspHeader = `upgrade-insecure-requests;`;
-
 const nextConfig = {
   basePath: process.env.BASE_PATH || '',
-  headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\n/g, ''),
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default withBundleAnalyzer(nextConfig);
