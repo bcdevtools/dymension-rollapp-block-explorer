9572600a26baf847ed24f6dcefc9633aad68bbe2
Teddy Nguyen <ducnh251092@gmail.com>
2024-05-01 21:34:44 +0700

    Query needed data only in transaction/account

--- web-app/src/services/db/chainInfo.ts
+++ web-app/src/services/db/chainInfo.ts
@@ -1,15 +1,25 @@
 import { cache } from 'react';
 import prisma from '../../utils/prisma';
-import { Prisma } from '@prisma/client';
+import { Prisma, chain_info } from '@prisma/client';
 
-export interface ChainInfo {
-  name: string;
-  chain_id: string;
-  be_json_rpc_urls: string[];
-  bech32: Prisma.JsonValue;
-  chain_type: string;
-  latest_indexed_block: bigint;
-}
+// export interface ChainInfo {
+//   name: string;
+//   chain_id: string;
+//   be_json_rpc_urls: string[];
+//   bech32: Prisma.JsonValue;
+//   chain_type: string;
+//   latest_indexed_block: bigint;
+// }
+
+export type ChainInfo = Pick<
+  chain_info,
+  | 'name'
+  | 'chain_id'
+  | 'be_json_rpc_urls'
+  | 'bech32'
+  | 'chain_type'
+  | 'latest_indexed_block'
+>;
 
 const select: Prisma.chain_infoSelect = {
   name: true,
