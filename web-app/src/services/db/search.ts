cd051881821f4b3d330237465ab878e2223ec511
Teddy Nguyen <ducnh251092@gmail.com>
2024-05-03 15:57:13 +0700

    Update search.ts

--- web-app/src/services/db/search.ts
+++ web-app/src/services/db/search.ts
@@ -20,22 +20,24 @@ export function searchBlock(value: string) {
 export const handleGlobalSearchOnServer = async function (value: string) {
   if (isTxHash(value)) {
     const txs = await searchByTxHash(value);
-    return { txs };
+    return txs.length ? { txs } : {};
   } else if (isEvmAddress(value)) {
     const evmRollapps = await getEvmChainInfo();
-    return { addess: evmRollapps };
+    return evmRollapps.length ? { addess: evmRollapps } : {};
   } else if (isCosmosAddress(value)) {
     const rollappAddress = RollappAddress.fromBech32(value);
     const result = await getChainInfoByPrefix(rollappAddress.prefix);
-    return { addess: result };
+    return result.length ? { addess: result } : {};
   } else {
     const result = await searchChainInfoByMultipleFields(value);
-    return {
-      chainNameOrChainIdOrBlock: result.map(i => ({
-        chain_id: i.chain_id,
-        name: i.name,
-        height: i.latest_indexed_block,
-      })),
-    };
+    return result
+      ? {
+          chainNameOrChainIdOrBlock: result.map(i => ({
+            chain_id: i.chain_id,
+            name: i.name,
+            height: i.latest_indexed_block,
+          })),
+        }
+      : {};
   }
 };
