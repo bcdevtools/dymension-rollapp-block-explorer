8ba49ac4c0593003ae9f43d80cbd07b62ae526e2
0xbcdev <0xbcdev@bcdev.tools>
2024-05-04 12:01:36 +0700

    increase number of msgs per tx

--- indexer/constants/validation.go
+++ indexer/constants/validation.go
@@ -3,7 +3,7 @@ package constants
 const (
 	RpcTxAheadBlockTimeAllowed                     = 86400 - 1 // now + X
 	RpcTxMaximumTxsPerBlockAllowed                 = 100       // maximum X txs per block
-	RpcTxMaximumMessagesPerTxAllowed               = 40        // maximum X msgs per tx
+	RpcTxMaximumMessagesPerTxAllowed               = 50        // maximum X msgs per tx
 	RpcTxMaximumStringSizeAllowed                  = 1000      // characters
 	RpcTxMaximumSignersPerTx                       = 10        // maximum X signers per tx
 	RpcTxMaximumLengthAddressAllowed               = 100       // address characters can not access X characters
