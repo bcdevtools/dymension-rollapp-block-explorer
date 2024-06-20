2fe61df1ff0f949a5a570dc7b533438fe7ce2461
0xbcdev <0xbcdev@bcdev.tools>
2024-06-21 03:24:17 +0700

    increase involvers per tx

--- indexer/constants/validation.go
+++ indexer/constants/validation.go
@@ -8,6 +8,6 @@ const (
 	RpcTxMaximumStringSizeAllowed                  = 1000      // characters
 	RpcTxMaximumSignersPerTx                       = 10        // maximum X signers per tx
 	RpcTxMaximumLengthAddressAllowed               = 100       // address characters can not access X characters
-	RpcTxMaximumInvolversPerTxAllowed              = 200       // no more than X involvers can be involved in one tx
+	RpcTxMaximumInvolversPerTxAllowed              = 250       // no more than X involvers can be involved in one tx
 	RpcTxMaximumTokenContractsInvolvesPerTxAllowed = 20        // no more than X token contracts can be involved in one tx
 )
